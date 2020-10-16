fdb96d1c38dfd70da146216a7d412d8ec686e73d
Paras Dandwani <dandwanip@gmail.com>
2020-10-17 00:41:35 +0530

    added quiz master and minor changes

--- js/index.js
+++ js/index.js
@@ -1,26 +1,35 @@
 function knowMore() {
+  document.getElementById("small").style.display = "block";
 
-    document.getElementById('small').style.display = "block";
+  document.getElementById("seeMore").style.display = "none";
 
-    document.getElementById('seeMore').style.display = "none";
-
-    document.getElementById('seeLess').style.display = "block";
-
+  document.getElementById("seeLess").style.display = "block";
 }
 function knowLess() {
+  document.getElementById("small").style.display = "none";
 
-    document.getElementById('small').style.display = "none";
+  document.getElementById("seeMore").style.display = "block";
+  document.getElementById("seeLess").style.display = "none";
+}
+function knowMore1() {
+  document.getElementById("small1").style.display = "block";
 
-    document.getElementById('seeMore').style.display = "block";
-    document.getElementById('seeLess').style.display = "none";
+  document.getElementById("seeMore1").style.display = "none";
 
+  document.getElementById("seeLess1").style.display = "block";
+}
+function knowLess1() {
+  document.getElementById("small1").style.display = "none";
+
+  document.getElementById("seeMore1").style.display = "block";
+  document.getElementById("seeLess1").style.display = "none";
 }
 $(document).ready(function () {
-    new WOW().init();
+  new WOW().init();
 });
 $("#disableNav").click(function () {
-    $("#hide").hide();
-})
-$('#myModal').on('hidden.bs.modal', function () {
-    $("#hide").show();
-})
+  $("#hide").hide();
+});
+$("#myModal").on("hidden.bs.modal", function () {
+  $("#hide").show();
+});
